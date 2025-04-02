3027779fe9f66a3cc3cd910711751639cd82419a
Anton Melnikov <melnikov.anton@gmail.com>
2025-04-02 13:08:32 +0300

    Made Button styles extendable

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,19 +1,26 @@
 // Lib
 // -----------------------------------------------------------------------------
 import type { FC, ReactNode } from "react";
+import clsx from "clsx";
 
 // Props
 // -----------------------------------------------------------------------------
 export interface ButtonProps {
   children: ReactNode;
+  className?: string;
 }
 
 /**
  * Button component.
  */
-export const Button: FC<ButtonProps> = ({ children }) => {
+export const Button: FC<ButtonProps> = ({ children, className }) => {
   return (
-    <button className="cursor-pointer rounded bg-blue-500 px-4 py-2 text-base text-white transition hover:bg-blue-400 focus:ring-4 focus:ring-blue-100 focus:outline-none disabled:cursor-not-allowed disabled:bg-slate-500 disabled:text-slate-300">
+    <button
+      className={clsx(
+        "cursor-pointer rounded bg-blue-500 px-4 py-2 text-base text-white transition hover:bg-blue-400 focus:ring-4 focus:ring-blue-100 focus:outline-none disabled:cursor-not-allowed disabled:bg-slate-500 disabled:text-slate-300",
+        className,
+      )}
+    >
       {children}
     </button>
   );
