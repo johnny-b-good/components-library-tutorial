2752e0897f45455748cebcaa09b7f18e2d017091
Anton Melnikov <melnikov.anton@gmail.com>
2025-04-03 13:30:25 +0300

    Button size variations

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -8,16 +8,25 @@ import clsx from "clsx";
 export interface ButtonProps {
   children: ReactNode;
   className?: string;
+  size?: "s" | "m" | "l" | "xl";
 }
 
 /**
  * Button component.
  */
-export const Button: FC<ButtonProps> = ({ children, className }) => {
+export const Button: FC<ButtonProps> = ({
+  children,
+  className,
+  size = "m",
+}) => {
   return (
     <button
       className={clsx(
-        "cursor-pointer rounded bg-blue-500 px-4 py-2 text-base text-white transition hover:bg-blue-400 focus:ring-4 focus:ring-blue-100 focus:outline-none disabled:cursor-not-allowed disabled:bg-slate-500 disabled:text-slate-300",
+        "cursor-pointer rounded bg-blue-500 font-semibold text-white transition hover:bg-blue-400 focus:ring-4 focus:ring-blue-100 focus:outline-none disabled:cursor-not-allowed disabled:bg-slate-500 disabled:text-slate-300",
+        size === "s" && "px-2 py-1.5 text-xs",
+        size === "m" && "px-2.5 py-2 text-sm",
+        size === "l" && "px-4 py-3 text-base",
+        size === "xl" && "px-8 py-4 text-xl",
         className,
       )}
     >
--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,11 @@ import { Button } from "./components";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <div className="margin-auto flex justify-center p-16">
-      <Button>Hello world!</Button>
+    <div className="margin-auto flex items-end justify-center gap-4 p-16">
+      <Button size="xl">Hello world!</Button>
+      <Button size="l">Hello world!</Button>
+      <Button size="m">Hello world!</Button>
+      <Button size="s">Hello world!</Button>
     </div>
   </StrictMode>,
 );
