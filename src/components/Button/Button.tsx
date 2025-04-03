35cc47245f4362a2fb98dc367599c0437c40d5e0
Anton Melnikov <melnikov.anton@gmail.com>
2025-04-03 14:01:42 +0300

    Button style variants

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -9,6 +9,7 @@ export interface ButtonProps {
   children: ReactNode;
   className?: string;
   size?: "s" | "m" | "l" | "xl";
+  variant?: "primary" | "default" | "text";
 }
 
 /**
@@ -18,15 +19,22 @@ export const Button: FC<ButtonProps> = ({
   children,
   className,
   size = "m",
+  variant = "default",
 }) => {
   return (
     <button
       className={clsx(
-        "cursor-pointer rounded bg-blue-500 font-semibold text-white transition hover:bg-blue-400 focus:ring-4 focus:ring-blue-100 focus:outline-none disabled:cursor-not-allowed disabled:bg-slate-500 disabled:text-slate-300",
+        "cursor-pointer rounded border font-semibold transition focus:ring-4 focus:ring-blue-100 focus:outline-none disabled:cursor-not-allowed disabled:bg-slate-500 disabled:text-slate-300",
         size === "s" && "px-2 py-1.5 text-xs",
         size === "m" && "px-2.5 py-2 text-sm",
         size === "l" && "px-4 py-3 text-base",
         size === "xl" && "px-8 py-4 text-xl",
+        variant === "primary" &&
+          "border-blue-500 bg-blue-500 text-white hover:bg-blue-400",
+        variant === "default" &&
+          "border-blue-500 bg-white text-blue-500 hover:bg-blue-50",
+        variant === "text" &&
+          "border-transparent bg-transparent text-blue-500 hover:bg-blue-50",
         className,
       )}
     >
--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,27 @@ import { Button } from "./components";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <div className="margin-auto flex items-end justify-center gap-4 p-16">
-      <Button size="xl">Hello world!</Button>
-      <Button size="l">Hello world!</Button>
-      <Button size="m">Hello world!</Button>
-      <Button size="s">Hello world!</Button>
+    <div className="margin-auto flex flex-col gap-8 p-16">
+      <div className="flex items-end justify-center gap-4">
+        <Button variant="primary" size="xl">
+          Hello world!
+        </Button>
+        <Button variant="primary" size="l">
+          Hello world!
+        </Button>
+        <Button variant="primary" size="m">
+          Hello world!
+        </Button>
+        <Button variant="primary" size="s">
+          Hello world!
+        </Button>
+      </div>
+
+      <div className="flex items-end justify-center gap-4">
+        <Button variant="primary">Hello world!</Button>
+        <Button variant="default">Hello world!</Button>
+        <Button variant="text">Hello world!</Button>
+      </div>
     </div>
   </StrictMode>,
 );
